c7e1c97df37ae3e959ee22600feb26daffbb303e
porschan <710437653@qq.com>
2019-06-16 23:50:05 +0800

    add huqiang web

---
+++ huqian/src/views/User.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div>User</div>
+        <div class="btn" @click="toAdd">add</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "User",
+        methods:{
+            toAdd(){
+                this.$router.push('/add')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
