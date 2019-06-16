257aae256711504593ec4d4ee6b48896939e2aaa
porschan <710437653@qq.com>
2019-06-16 23:59:14 +0800

    update something

---
+++ huqian/project/js/index.js
@@ -0,0 +1,287 @@
+$(function () {
+  // var data = [{
+  //   "name": "中国队必胜",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/9DE656A9B0C0384FCCF7D02BD02CFCB5/100",
+  //   "money": "25.25"
+  // },
+  // {
+  //   "name": "刘子怡爸爸",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/D091A297D0A3D3619C6D828C681F305F/100",
+  //   "money": "23.36"
+  // },
+  // {
+  //   "name": "我是你的情人",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/685AA36438DDD7E0EB55D0C18097CA1C/100",
+  //   "money": "28.79"
+  // },
+  // {
+  //   "name": "花花公子",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/DDA36344FDAF8DF2BFDD8F3DAEDE5B74/100",
+  //   "money": "19.39"
+  // },
+  // {
+  //   "name": "潇了个洒",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/E2348DFF85AE861D17451BDDC0432809/100",
+  //   "money": "23.90"
+  // },
+  // {
+  //   "name": "天天向上",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/1656EDDA7E648DD32E862460EE92E1C5/100",
+  //   "money": "22.04"
+  // },
+  // {
+  //   "name": "一刘阿姨",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/29DBC6217FA0B06ABC25C70FE260221F/100",
+  //   "money": "25.56"
+  // },
+  // {
+  //   "name": "孔海西",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/72763DE05338B738EEA4D9FBEFD8DBBF/100",
+  //   "money": "13.67"
+  // },
+  // {
+  //   "name": "毕竟我们还年轻",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/A6F3CA4B97E59BB9AE5495984ACF3090/100",
+  //   "money": "24.57"
+  // },
+  // {
+  //   "name": "世界杯看客",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/0DE079B903E44F96AB9BAD85D706A61F/100",
+  //   "money": "22.61"
+  // },
+  // {
+  //   "name": "频繁的我",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/94B9F8421330A7B82F019492C822BF42/100",
+  //   "money": "25.81"
+  // },
+  // {
+  //   "name": "妙不可言",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/D56EE4D71422A112CDA6B7B44D48B044/100",
+  //   "money": "29.18"
+  // },
+  // {
+  //   "name": "春天般～温暖",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/FF4E560E4F11C2EBAAFFFC4625CD3122/100",
+  //   "money": "21.65"
+  // },
+  // {
+  //   "name": "小猫咪回忆",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/F6213667E85E205FF363B3947D218D38/100",
+  //   "money": "14.02"
+  // },
+  // {
+  //   "name": "10G流量不够用",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/D42066DE19EBB82D30A351185956DB41/100",
+  //   "money": "26.11"
+  // },
+  // {
+  //   "name": "梅西王子",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/5DA508A1616E732B0EB92A1ADAF28456/100",
+  //   "money": "24.74"
+  // },
+  // {
+  //   "name": "中国队必胜",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/9DE656A9B0C0384FCCF7D02BD02CFCB5/100",
+  //   "money": "25.25"
+  // },
+  // {
+  //   "name": "刘子怡爸爸",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/D091A297D0A3D3619C6D828C681F305F/100",
+  //   "money": "23.36"
+  // },
+  // {
+  //   "name": "我是你的情人",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/685AA36438DDD7E0EB55D0C18097CA1C/100",
+  //   "money": "28.79"
+  // },
+  // {
+  //   "name": "花花公子",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/DDA36344FDAF8DF2BFDD8F3DAEDE5B74/100",
+  //   "money": "19.39"
+  // },
+  // {
+  //   "name": "潇了个洒",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/E2348DFF85AE861D17451BDDC0432809/100",
+  //   "money": "23.90"
+  // },
+  // {
+  //   "name": "天天向上",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/1656EDDA7E648DD32E862460EE92E1C5/100",
+  //   "money": "22.04"
+  // },
+  // {
+  //   "name": "一刘阿姨",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/29DBC6217FA0B06ABC25C70FE260221F/100",
+  //   "money": "25.56"
+  // },
+  // {
+  //   "name": "孔海西",
+  //   "avatar": "https://q.qlogo.cn/qqapp/1104718115/72763DE05338B738EEA4D9FBEFD8DBBF/100",
+  //   "money": "13.67"
+  // },
+  // ]
+  
+  
+
+
+  var data = [{
+    "name": "中国队必胜",
+    "avatar": "https://q.qlogo.cn/qqapp/1104718115/9DE656A9B0C0384FCCF7D02BD02CFCB5/100",
+    "money": "25.25"
+  },
+  {
+    "name": "刘子怡爸爸",
+    "avatar": "https://q.qlogo.cn/qqapp/1104718115/D091A297D0A3D3619C6D828C681F305F/100",
+    "money": "23.36"
+  },
+  {
+    "name": "我是你的情人",
+    "avatar": "https://q.qlogo.cn/qqapp/1104718115/685AA36438DDD7E0EB55D0C18097CA1C/100",
+    "money": "28.79"
+  },
+  {
+    "name": "花花公子",
+    "avatar": "https://q.qlogo.cn/qqapp/1104718115/DDA36344FDAF8DF2BFDD8F3DAEDE5B74/100",
+    "money": "19.39"
+  },
+  {
+    "name": "潇了个洒",
+    "avatar": "https://q.qlogo.cn/qqapp/1104718115/E2348DFF85AE861D17451BDDC0432809/100",
+    "money": "23.90"
+  },
+  {
+    "name": "天天向上",
+    "avatar": "https://q.qlogo.cn/qqapp/1104718115/1656EDDA7E648DD32E862460EE92E1C5/100",
+    "money": "22.04"
+  },
+  {
+    "name": "一刘阿姨",
+    "avatar": "https://q.qlogo.cn/qqapp/1104718115/29DBC6217FA0B06ABC25C70FE260221F/100",
+    "money": "25.56"
+  },
+  {
+    "name": "孔海西",
+    "avatar": "https://q.qlogo.cn/qqapp/1104718115/72763DE05338B738EEA4D9FBEFD8DBBF/100",
+    "money": "13.67"
+    },
+    {
+      "name": "天天向上",
+      "avatar": "https://q.qlogo.cn/qqapp/1104718115/1656EDDA7E648DD32E862460EE92E1C5/100",
+      "money": "22.04"
+    },
+    {
+      "name": "一刘阿姨",
+      "avatar": "https://q.qlogo.cn/qqapp/1104718115/29DBC6217FA0B06ABC25C70FE260221F/100",
+      "money": "25.56"
+    }
+  ];
+  var html = '', num = 7;
+  //随机获取时间
+  function time() {
+    //随机数
+    let randomNum = Math.floor((Math.random() * 30) + 1)
+    //获取当前分钟
+    const date = new Date()
+    let h = date.getHours();
+    let m = date.getMinutes();
+    if (randomNum > m) {
+      h = h < 10 ? '0' + h : h--;
+      m = 60 - (randomNum - m)
+      return `${h}:${m < 10 ? '0' + m : m}`
+    } else {
+      return `${h}:${randomNum < 10 ? '0' + randomNum : randomNum}`
+    }
+  };
+  //遍历生成HTML字符串
+  for (var i = 0; i < data.length; i++) {
+    html += ` <li style="display:flex">
+    <div class="money_user_wrap">
+      <span class="user_icon">
+        <img src="${data[i].avatar}" alt=""/>
+      </span>
+      <div class="user_info">
+        <div class="user_name">${data[i].name}</div>
+        <div class="user_time">${time()}</div>
+      </div>
+    </div>
+    <div class="recive_money_wrap">
+      <span class="recive_money_text">领取</span>
+      <span class="recive_money_nums">${data[i].money}元</span>
+    </div>
+  </li>`
+  }
+  //渲染页面
+  $('.hiBox ul').html(html)
+  var height = Math.floor($('.hiBox ul>li').height());
+  // 重置前7个和后7个的时间
+  var timeTexts = $('.hiBox ul>li .user_time')
+  for (let i = 0; i < timeTexts.length; i++) {
+    if (i < num) {
+      timeTexts[i].innerText = timeTexts[timeTexts.length + i - num].innerText
+    }
+  }
+  //设置每个li的高度
+  $('.hiBox ul>li').css({
+    height: (height * 1.5),
+  })
+  //动态设置容器高度
+  $('.hiBox').css({
+    height: (height * 1.5) * num,
+  })
+  //设置初始化位置
+  // $('.footer ul').css({
+  //   top: `-${(data.length - num) * (height * 1.5)}px`
+  // })
+  //定时器实现切换
+
+  var arr = [];
+  var j = 0;
+  for (var i = 0; i < data.length; i++) { 
+
+    html = ` <li style="display:flex">
+    <div class="money_user_wrap">
+      <span class="user_icon">
+        <img src="${data[i].avatar}" alt=""/>
+      </span>
+      <div class="user_info">
+        <div class="user_name">${data[i].name}</div>
+        <div class="user_time">${time()}</div>
+      </div>
+    </div>
+    <div class="recive_money_wrap">
+      <span class="recive_money_text">领取</span>
+      <span class="recive_money_nums">${data[i].money}元</span>
+    </div>
+  </li>`
+    arr.push(html);
+  }
+
+
+  $('.footer ul ').clone().insertBefore($('.footer ul'))
+  // clearInterval(timer);
+
+  $('.hiBox ul').html("")
+  var timer = setInterval(() => {
+   
+    $('.footer ul').prepend(arr[j]);
+    $('.footer ul').children().eq(j).fadeIn(1000);
+    console.log($('.footer ul').children().eq(j))
+    j++;
+    if (j == data.length) {
+      j = 0;
+      clearInterval(timer);
+    }
+  }, 0)
+  //  start - edit 20190616
+  // var t = 0;
+  // setInterval(() => {
+    
+  //   t++
+  //   $('.footer ul').css('top',t)
+  //  },50)
+  //  end - edit 20190616
+})
+
+
+
+
